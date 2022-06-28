3390f1dea83631b38ff484325fc40318883f8139
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2022-06-28 14:06:28 -0400

    Remove AuthN
    Configure authorization and graphql endpoint to be used with an external authentication endpoint

--- config.js
+++ config.js
@@ -4,13 +4,6 @@ dotenv.config();
 const config = {
   version: process.env.VERSION,
   date: process.env.DATE,
-  idp: process.env.IDP ? process.env.IDP.toLowerCase() : 'google',
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  redirect_url: process.env.REDIRECT_URL,
-  cookie_secret: process.env.COOKIE_SECRET,
-  session_timeout: process.env.SESSION_TIMEOUT ? parseInt(process.env.SESSION_TIMEOUT) : 30 * 60,  // 30 minutes
-  authorization_enabled: process.env.AUTHORIZATION_ENABLED ? process.env.AUTHORIZATION_ENABLED.toLowerCase() === 'true' : true,
   emails_enabled: process.env.EMAILS_ENABLED ? process.env.EMAILS_ENABLED.toLowerCase() === 'true' : true,
 
   //Neo4j connection
@@ -22,29 +15,7 @@ const config = {
   DATA_FILE: process.env.DATA_FILE,
   //Testing
   TEST_EMAIL: process.env.TEST_EMAIL,
-  google: {
-    CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
-  },
-  nih: {
-    CLIENT_ID: process.env.NIH_CLIENT_ID,
-    CLIENT_SECRET: process.env.NIH_CLIENT_SECRET,
-    BASE_URL: process.env.NIH_BASE_URL,
-    REDIRECT_URL: process.env.NIH_REDIRECT_URL,
-    USERINFO_URL: process.env.NIH_USERINFO_URL,
-    AUTHORIZE_URL: process.env.NIH_AUTHORIZE_URL,
-    TOKEN_URL: process.env.NIH_TOKEN_URL,
-    LOGOUT_URL: process.env.NIH_LOGOUT_URL,
-    SCOPE: process.env.NIH_SCOPE,
-    PROMPT: process.env.NIH_PROMPT
-  },
-  // MySQL Session
-  mysql_host: process.env.MYSQL_HOST,
-  mysql_port: process.env.MYSQL_PORT,
-  mysql_user: process.env.MYSQL_USER,
-  mysql_password: process.env.MYSQL_PASSWORD,
-  mysql_database: process.env.MYSQL_DATABASE,
+
   // Email settings
   email_service_email: process.env.EMAIL_SERVICE_EMAIL,
   email_transport: getTransportConfig()
