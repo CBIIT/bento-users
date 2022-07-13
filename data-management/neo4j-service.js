94d589d32dd9cb27df5fac044b14ee0a227fe253
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2022-07-13 11:37:51 -0400

    Updated listUsers function to match schema changes

--- data-management/neo4j-service.js
+++ data-management/neo4j-service.js
@@ -64,15 +64,37 @@ async function listUsers(parameters) {
     const cypher =
     `
         MATCH (user:User)
-        WHERE user.role IN $role AND user.status IN $status
-        return user 
+        WHERE user.role IN $role AND user.userStatus IN $userStatus
+        MATCH (user)<-[:of_user]-(request:Access)
+        WHERE request.accessStatus IN $accessStatus
+        OPTIONAL MATCH (reviewer:User)<-[:approved_by]-(request)
+        OPTIONAL MATCH (arm:Arm)<-[:of_arm]-(request)
+        WITH user, COLLECT(DISTINCT request{
+            armID: arm.armID,
+            armName: arm.armName,
+            status: request.accessStatus,
+            requestDate: request.requestDate,
+            reviewAdminName: reviewer.firstName + " " + reviewer.lastName,
+            reviewDate: request.reviewDate,
+            comment: request.comment
+        }) as acl
+        RETURN {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            organization: user.organization,
+            userID: user.userID,
+            email: user.email,
+            IDP: user.IDP,
+            role: user.role,
+            status: user.userStatus,
+            creationDate: user.creationDate,
+            editDate: user.editDate,
+            acl: acl
+        } AS user
     `
     const users = []
     const result = await executeQuery(parameters, cypher, 'user');
-    result.forEach(x => {
-        users.push(x.properties)
-    });
-    return users;
+    return result;
 }
 
 async function listArms(parameters) {
