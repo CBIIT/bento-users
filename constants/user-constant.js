362c08a8d8e9868a6b4049a923d4424dad8b12b1
knockknockyoo <iheaven0129@gmail.com>
2022-07-14 15:29:27 -0400

    added active user

--- constants/user-constant.js
+++ constants/user-constant.js
@@ -2,6 +2,7 @@ module.exports = Object.freeze({
     // user status
     NONE: '',
     INACTIVE: 'inactive',
+    ACTIVE: 'active',
     DISABLED: 'disabled',
     DELETED: 'deleted',
     // user role
