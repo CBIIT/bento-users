c26062fd46641e98b694410f4d40c2761ea94434
knockknockyoo <iheaven0129@gmail.com>
2022-07-20 17:01:20 -0400

    updated test codes

--- test/arm/arm-access.test.js
+++ test/arm/arm-access.test.js
@@ -1,37 +1,11 @@
 const ArmAccess = require("../../model/arm-access");
-const {APPROVED, REQUESTED, REJECTED} = require("../../constants/access-constant");
+const {REQUESTED} = require("../../constants/access-constant");
 describe('register user API test', () => {
 
     test('/request access', ()=> {
-        const result = ArmAccess.createRequestAccess('a', ['armA']);
+        const result = ArmAccess.createRequestAccess();
         expect(result).toBeTruthy();
-        expect(result.getArmAccess().userID).toBe('a');
-        expect(result.getArmAccess().armIds).toStrictEqual(['armA']);
-        expect(result.getArmAccess().accessStatus).toBe(REQUESTED);
+        expect(result.getAccessStatus()).toBe(REQUESTED);
 
-    });
-
-    test('/approval access', ()=> {
-        const arm = ArmAccess.createApprovalAccess('admin', 'requestID','thanks');
-        const result = arm.getArmAccess();
-        expect(result).toBeTruthy();
-        expect(result.approvedBy).toBe('admin');
-        expect(result.requestID).toBe('requestID');
-        expect(result.accessStatus).toBe(APPROVED);
-        expect(result.reviewDate).toBeTruthy();
-        expect(result.userID).toBe('');
-        expect(result.comment).toBe('thanks');
-    });
-
-    test('/reject access', ()=> {
-        const arm = ArmAccess.createRejectAccess('requestID');
-        const result = arm.getArmAccess();
-        expect(result).toBeTruthy();
-        expect(result.approvedBy).toBe('');
-        expect(result.requestID).toBe('requestID');
-        expect(result.accessStatus).toBe(REJECTED);
-        expect(result.reviewDate).toBeTruthy();
-        expect(result.userID).toBe('');
-        expect(result.comment).toBe('');
     });
 });
