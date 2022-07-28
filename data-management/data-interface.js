5043d3df25a7057236f6fa1d754492f48ced60c1
knockknockyoo <iheaven0129@gmail.com>
2022-07-28 12:48:06 -0400

    removed task, execute func

--- data-management/data-interface.js
+++ data-management/data-interface.js
@@ -62,20 +62,17 @@ const validator = {
 }
 
 const getMyUser = async (_, context) => {
-    const task = async () => {
-        if (!verifyUserInfo(context.userInfo)) throw new Error(errorName.NOT_LOGGED_IN);
-        let result = await neo4j.getMyUser(context.userInfo);
-        // store user if not exists in db
-        if (!result) {
-            saveUserInfoToSession(context, context.userInfo);
-            // no email notification for auto-generated user
-            const user = UserBuilder.createUser(context.userInfo);
-            return await registerUser({ userInfo: user.getUserInfo(), isNotify: false }, context);
-        }
-        saveUserInfoToSession(context, result);
-        return result;
+    if (!verifyUserInfo(context.userInfo)) throw new Error(errorName.NOT_LOGGED_IN);
+    let result = await neo4j.getMyUser(context.userInfo);
+    // store user if not exists in db
+    if (!result) {
+        saveUserInfoToSession(context, context.userInfo);
+        // no email notification for auto-generated user
+        const user = UserBuilder.createUser(context.userInfo);
+        return await registerUser({ userInfo: user.getUserInfo(), isNotify: false }, context);
     }
-    return await execute(task);
+    saveUserInfoToSession(context, result);
+    return result;
 }
 
 const getUser = async (parameters, context) => {
