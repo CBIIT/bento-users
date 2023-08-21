7c68b92f83f22f75f41b8e55825152322b8de14c
huuaho <32457341+huuaho@users.noreply.github.com>
2023-08-21 16:38:38 -0400

    set email enabled to false

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const config = {
   version: process.env.VERSION,
   date: process.env.DATE,
-  emails_enabled: process.env.EMAILS_ENABLED ? process.env.EMAILS_ENABLED.toLowerCase() === 'true' : true,
+  emails_enabled: false,
   inactive_user_days : process.env.INACTIVE_USER_DAYS ? process.env.INACTIVE_USER_DAYS : 60,
   cookie_secret: process.env.COOKIE_SECRET,
   session_timeout: process.env.SESSION_TIMEOUT ? parseInt(process.env.SESSION_TIMEOUT) * 1000 : 1000 * 30 * 60,  // 30 minutes
